3545663bba46e3ce103cb642a2e0b2905ca0c224
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-17 23:57:38 +0300

    Add basic routing (home, login, register)

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('../app/features/home/home').then(comp => comp.Home) },
+    { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login) },
+    { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register) },
+];
