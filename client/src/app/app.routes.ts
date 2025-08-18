c40abf1d6e08a3ab5559ffdfd9517b2677441d7e
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-18 11:54:10 +0300

    Add edit and details routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,6 +6,8 @@ export const routes: Routes = [
     { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login) },
     { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register) },
     { path: 'listings', loadComponent: () => import('../app/features/listings/catalog/catalog').then(comp => comp.Catalog) },
+    { path: 'listings/:listingId/details', loadComponent: () => import('../app/features/listings/listing-details/listing-details').then(comp => comp.ListingDetails) },
+    { path: 'listings/:listingId/edit', loadComponent: () => import('../app/features/listings/edit/edit').then(comp => comp.Edit) },
     { path: 'listings/create', loadComponent: () => import('../app/features/listings/create/create').then(comp => comp.Create) },
     { path: 'search', loadComponent: () => import('../app/features/listings/search/search').then(comp => comp.Search) }
 ];
