66a047168fbee0b79935715b1fea6afc8d7499a3
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-18 11:22:44 +0300

    Add more routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,4 +5,6 @@ export const routes: Routes = [
     { path: 'home', loadComponent: () => import('../app/features/home/home').then(comp => comp.Home) },
     { path: 'login', loadComponent: () => import('../app/features/auth/login/login').then(comp => comp.Login) },
     { path: 'register', loadComponent: () => import('../app/features/auth/register/register').then(comp => comp.Register) },
+    { path: 'listings', loadComponent: () => import('../app/features/listings/catalog/catalog').then(comp => comp.Catalog) },
+    { path: 'listings/create', loadComponent: () => import('../app/features/listings/create/create').then(comp => comp.Create) },
 ];
