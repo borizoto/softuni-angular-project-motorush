ddeb96b0f7e60250a971be9b9b1d11266f57998b
Boris Kalapchiev <boriskalapchiev@gmail.com>
2025-08-19 18:38:35 +0300

    Check if isOnwer on details page

--- client/src/app/features/listings/listing-details/listing-details.ts
+++ client/src/app/features/listings/listing-details/listing-details.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Motorbike } from '../../../models';
 import { ListingService } from '../../../core/services/listings.service';
 import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
 	selector: 'app-listing-details',
@@ -11,11 +12,23 @@ import { AsyncPipe } from '@angular/common';
 	templateUrl: './listing-details.html',
 	styleUrl: './listing-details.css'
 })
-export class ListingDetails {
+export class ListingDetails implements OnInit {
 	$listing!: Observable<Motorbike>;
+	isOwner = false;
 
-	constructor(private listingsService: ListingService, private route: ActivatedRoute) {
+	constructor(
+		private listingsService: ListingService,
+		private route: ActivatedRoute,
+		private authService: AuthService
+	) { }
+
+	ngOnInit(): void {
 		const listingId = this.route.snapshot.paramMap.get('listingId')!;
 		this.$listing = this.listingsService.getOne(listingId);
+
+		this.$listing.subscribe(listing => {
+			const currentUser = this.authService.currentUser();
+			this.isOwner = currentUser?._id === listing._ownerId;
+		});
 	}
 }
